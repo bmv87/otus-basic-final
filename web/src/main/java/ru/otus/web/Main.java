356c0f1b315e9aeff6c7f81b83bcc45b27f826aa
BMV <venerasmith@mail.ru>
2024-08-14 14:21:35 +0300

    feat: app argument -port

--- web/src/main/java/ru/otus/web/Main.java
+++ web/src/main/java/ru/otus/web/Main.java
@@ -1,8 +1,24 @@
 package ru.otus.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.web.helpers.ApplicationArgumentsHelper;
+
 public class Main {
+    private static final String PORT_ARG_NAME = "-port";
+    private static final int DEFAULT_PORT = 8189;
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws Exception {
-        try (var server = new HttpServer(8189)) {
+        ApplicationArgumentsHelper.tryParse(args);
+        int port = DEFAULT_PORT;
+        try {
+            port = ApplicationArgumentsHelper.tryGet(PORT_ARG_NAME, Integer.class);
+        } catch (RuntimeException e) {
+            logger.info(e.getMessage());
+        }
+
+        try (var server = new HttpServer(port)) {
             server.start();
         }
     }
